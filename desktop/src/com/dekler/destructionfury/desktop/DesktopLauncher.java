af665c7f3aa7d429d30d8e019ed70fa957ebb970
GSamuel <gideon_hoog@hotmail.com>
2015-03-26 12:41:42 +0100

    first commit

---
+++ desktop/src/com/dekler/destructionfury/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dekler.destructionfury.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dekler.destructionfury.DestructionFury;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DestructionFury(), config);
+	}
+}
